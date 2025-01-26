ebe92c2faf18f61a72ed2eff2cf1af9ed657dfc6
tyhh00 <tanyonghong.prv@gmail.com>
2025-01-26 12:21:05 +0800

    trying to get logs to display

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -12,6 +12,7 @@ module.exports = {
       max_memory_restart: '1G',
       error_file: 'logs/err.log',
       out_file: 'logs/out.log',
+      merge_logs: true,
       time: true,
       instances: 1,
       autorestart: true,
@@ -19,7 +20,8 @@ module.exports = {
       max_restarts: 10,
       restart_delay: 5000,
       env_file: '.env',
-      log_date_format: 'YYYY-MM-DD HH:mm:ss Z'
+      log_date_format: 'YYYY-MM-DD HH:mm:ss Z',
+      log_type: 'json'
     }
   ]
 } 
